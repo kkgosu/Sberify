c7d70502a8a4ab37b65a77c7e4c4e2c751191bd4
kkgosu <kostya96.08@mail.ru>
2019-12-05 13:57:34 +0300

    new releases switched to data binding

--- app/src/main/java/com/example/sberify/presentation/ui/newreleases/NewReleasesAdapter.kt
+++ app/src/main/java/com/example/sberify/presentation/ui/newreleases/NewReleasesAdapter.kt
@@ -1,15 +1,15 @@
 package com.example.sberify.presentation.ui.newreleases
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sberify.R
+import com.example.sberify.databinding.ItemAlbumBinding
 import com.example.sberify.domain.model.Album
 import com.example.sberify.presentation.ui.utils.createDiffCallback
-import com.example.sberify.presentation.ui.utils.inflateLayout
-import com.example.sberify.presentation.ui.utils.loadImage
-import kotlinx.android.synthetic.main.item_album.view.*
 
 class NewReleasesAdapter(
         private val interaction: Interaction? = null) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -17,13 +17,19 @@ class NewReleasesAdapter(
     private val DIFF_CALLBACK = createDiffCallback<Album>()
     private val differ = AsyncListDiffer(this, DIFF_CALLBACK)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
-            ViewHolder(inflateLayout(R.layout.item_album, parent), interaction)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val newReleasesBinding: ItemAlbumBinding =
+                DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.item_album,
+                        parent, false)
+        return ViewHolder((newReleasesBinding), interaction)
+    }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
             is ViewHolder -> {
-                holder.bind(differ.currentList.get(position))
+                val album = differ.currentList[position]
+                holder.bindingItem.album = album
+                holder.bind(album)
             }
         }
     }
@@ -37,20 +43,20 @@ class NewReleasesAdapter(
     }
 
     class ViewHolder
-    constructor(itemView: View, private val interaction: Interaction?) : RecyclerView.ViewHolder(
-            itemView) {
-
+    constructor(val bindingItem: ItemAlbumBinding, private val interaction: Interaction?) : RecyclerView.ViewHolder(
+            bindingItem.root) {
+        
         fun bind(item: Album) = with(itemView) {
             itemView.setOnClickListener {
                 interaction?.onItemSelected(item, this)
             }
-            release_cover.loadImage(item.imageUrl)
+/*            release_cover.loadImage(item.imageUrl)
             release_name.text = item.name
             artist_name.text = item.artist.name
 
             release_cover.transitionName = item.id
             release_name.transitionName = item.name
-            artist_name.transitionName = item.artist.name
+            artist_name.transitionName = item.artist.name*/
         }
     }
 
--- app/src/main/java/com/example/sberify/presentation/ui/utils/ViewUtils.kt
+++ app/src/main/java/com/example/sberify/presentation/ui/utils/ViewUtils.kt
@@ -3,9 +3,11 @@ package com.example.sberify.presentation.ui.utils
 import android.app.Activity
 import android.view.View
 import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.Target
 
+@BindingAdapter("image")
 fun ImageView.loadImage(image: String?) = ifNotDestroyed {
     Glide.with(this).load(image).override(Target.SIZE_ORIGINAL).centerCrop().into(this)
 }
