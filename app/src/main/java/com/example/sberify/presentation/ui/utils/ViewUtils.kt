b96264b46b1ff91654a7ca868349b6524b19d460
kkgosu <kostya96.08@mail.ru>
2020-03-10 22:16:03 +0300

    added new album detail layout

--- app/src/main/java/com/example/sberify/presentation/ui/utils/ViewUtils.kt
+++ app/src/main/java/com/example/sberify/presentation/ui/utils/ViewUtils.kt
@@ -1,9 +1,13 @@
 package com.example.sberify.presentation.ui.utils
 
 import android.app.Activity
+import android.graphics.Color
+import android.transition.TransitionManager
 import android.view.View
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -11,20 +15,27 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.target.Target
+import com.example.sberify.R
 import com.example.sberify.models.domain.Track
 import com.github.florent37.glidepalette.BitmapPalette
 import com.github.florent37.glidepalette.GlidePalette
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.transition.MaterialArcMotion
+import com.google.android.material.transition.MaterialContainerTransform
+import kotlin.math.abs
 
 @BindingAdapter("image")
 fun ImageView.loadImage(image: String?) = ifNotDestroyed {
     image?.let {
         Glide.with(this)
-                .load(it)
-                .transition(DrawableTransitionOptions.withCrossFade(375))
-                .override(Target.SIZE_ORIGINAL)
-                .centerCrop()
-                .into(this)
+            .load(it)
+            .transition(DrawableTransitionOptions.withCrossFade(375))
+            .override(Target.SIZE_ORIGINAL)
+            .centerCrop()
+            .into(this)
     }
 }
 
@@ -41,13 +52,73 @@ fun CollapsingToolbarLayout.setTitle(track: Track?) {
 fun ImageView.bindingPalette(path: String?, palette: View) {
     path?.let {
         Glide.with(this.context)
-                .load(path)
-                .transition(DrawableTransitionOptions.withCrossFade(375))
-                .listener(GlidePalette.with(path)
-                        .use(BitmapPalette.Profile.VIBRANT_DARK)
-                        .intoBackground(palette)
-                        .crossfade(true))
-                .into(this)
+            .load(path)
+            .transition(DrawableTransitionOptions.withCrossFade(375))
+            .listener(
+                GlidePalette.with(path)
+                    .use(BitmapPalette.Profile.VIBRANT_DARK)
+                    .intoBackground(palette)
+                    .crossfade(true)
+            )
+            .into(this)
+    }
+}
+
+@BindingAdapter("bindFab")
+fun bindAppBarLayoutWithFab(appBarLayout: AppBarLayout, fab: FloatingActionButton) {
+    appBarLayout.addOnOffsetChangedListener(
+        AppBarLayout.OnOffsetChangedListener { appBarLayout1, verticalOffset ->
+            val verticalOffsetPercentage = abs(
+                verticalOffset
+            ).toFloat() / appBarLayout1.totalScrollRange.toFloat()
+            if (verticalOffsetPercentage > 0.4 && fab.isOrWillBeShown) {
+                fab.hide()
+            } else if (verticalOffsetPercentage <= 0.4f && fab.isOrWillBeHidden && fab.tag != View.GONE) {
+                fab.show()
+            }
+        }
+    )
+}
+
+@BindingAdapter("transformFab", "transformContainer")
+fun bindTransformFab(view: View, fab: FloatingActionButton, container: CoordinatorLayout) {
+    fab.setOnClickListener {
+        fab.tag = View.GONE
+        TransitionManager.beginDelayedTransition(container, getTransform(view, fab))
+        fab.visibility = View.GONE
+        view.visibility = View.VISIBLE
+    }
+
+    view.setOnClickListener {
+        fab.tag = View.VISIBLE
+        TransitionManager.beginDelayedTransition(container, getTransform(view, fab))
+        fab.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+    }
+}
+
+fun AppCompatActivity.simpleToolbarWithHome(toolbar: MaterialToolbar, title_: String = "") {
+    setSupportActionBar(toolbar)
+    supportActionBar?.run {
+        setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+        setDisplayHomeAsUpEnabled(true)
+        title = title_
+    }
+}
+
+@BindingAdapter("simpleToolbarWithHome", "simpleToolbarTitle")
+fun bindToolbarWithTitle(toolbar: MaterialToolbar, activity: AppCompatActivity, title: String) {
+    activity.simpleToolbarWithHome(toolbar, title)
+}
+
+internal fun getTransform(mStartView: View, mEndView: View): MaterialContainerTransform {
+    return MaterialContainerTransform(mStartView.context).apply {
+        startView = mStartView
+        endView = mEndView
+        pathMotion = MaterialArcMotion()
+        duration = 650
+        scrimColor = Color.TRANSPARENT
     }
 }
 
