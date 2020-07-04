a492a2294bf3661e00d04683db5283a9fddfde47
kkgosu <kostya96.08@mail.ru>
2020-07-04 19:55:00 +0300

    test fixes

--- app/src/test/java/com/example/sberify/presentation/ui/SharedViewModelTest.kt
+++ app/src/test/java/com/example/sberify/presentation/ui/SharedViewModelTest.kt
@@ -13,7 +13,6 @@ import com.example.sberify.models.domain.Album
 import com.example.sberify.models.domain.Artist
 import com.example.sberify.models.domain.Suggestion
 import com.example.sberify.models.domain.Track
-import com.example.sberify.presentation.ui.search.SearchType
 import com.nhaarman.mockitokotlin2.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
@@ -72,7 +71,8 @@ class SharedViewModelTest {
     fun getArtist() = runBlocking {
         val observer = mock<Observer<Result<List<Artist>>>>()
         sharedViewModel.artistsSearchResult.observeForever(observer)
-        sharedViewModel.search("1", SearchType.ARTIST)
+        sharedViewModel.isArtistChecked = true
+        sharedViewModel.search("1")
         verify(spotifyRepository).searchArtist("1")
         verifyNoMoreInteractions(spotifyRepository)
         return@runBlocking
@@ -82,7 +82,8 @@ class SharedViewModelTest {
     fun getAlbums() = runBlocking {
         val observer = mock<Observer<Result<List<Album>>>>()
         sharedViewModel.albumsSearchResult.observeForever(observer)
-        sharedViewModel.search("1", SearchType.ALBUM)
+        sharedViewModel.isAlbumChecked = true
+        sharedViewModel.search("1")
         verify(spotifyRepository).searchAlbum("1")
         return@runBlocking
     }
@@ -91,7 +92,8 @@ class SharedViewModelTest {
     fun getTracks() = runBlocking {
         val observer = mock<Observer<Result<List<Track>>>>()
         sharedViewModel.tracksSearchResult.observeForever(observer)
-        sharedViewModel.search("1", SearchType.TRACK)
+        sharedViewModel.isTrackChecked = true
+        sharedViewModel.search("1")
         verify(spotifyRepository).searchTrack("1")
         return@runBlocking
     }
@@ -103,7 +105,7 @@ class SharedViewModelTest {
         sharedViewModel.lyrics.observeForever(observer)
 
         whenever(geniusRepository.getLyrics(track))
-                .thenReturn(MutableLiveData(Result.success(track)))
+            .thenReturn(MutableLiveData(Result.success(track)))
 
         sharedViewModel.getLyrics(track)
         verify(geniusRepository, only()).getLyrics(track)
@@ -124,9 +126,11 @@ class SharedViewModelTest {
         val observer3 = mock<Observer<Result<List<Artist>>>>()
         sharedViewModel.artistsSearchResult.observeForever(observer3)
 
-        sharedViewModel.search("key", SearchType.ARTIST)
-        sharedViewModel.search("key", SearchType.ALBUM)
-        sharedViewModel.search("key", SearchType.TRACK)
+        sharedViewModel.isTrackChecked = true
+        sharedViewModel.isAlbumChecked = true
+        sharedViewModel.isArtistChecked = true
+
+        sharedViewModel.search("key")
 
         val inOrder = inOrder(spotifyRepository)
         inOrder.apply {
@@ -141,7 +145,8 @@ class SharedViewModelTest {
         val observer = mock<Observer<Result<List<Album>>>>()
 
         whenever(spotifyRepository.getNewReleases()).thenReturn(
-                MutableLiveData(Result.success(listOf(mockAlbum()))))
+            MutableLiveData(Result.success(listOf(mockAlbum())))
+        )
 
         sharedViewModel.newReleases.observeForever(observer)
         sharedViewModel.refresh()
@@ -184,11 +189,11 @@ class SharedViewModelTest {
         val lyrics = mock<MutableLiveData<Result<Track>>>()
 
         whenever(lyrics.value)
-                .thenReturn(result1)
-                .thenReturn(result2)
+            .thenReturn(result1)
+            .thenReturn(result2)
 
         sharedViewModel = SharedViewModel(spotifyRepository, geniusRepository, databaseRepository)
-        
+
         val initialValue = lyrics.value
         sharedViewModel.refreshLyrics()
         val newValue = lyrics.value
