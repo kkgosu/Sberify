748d1f3190a76d386ae49f051719bc559db917a7
kkgosu <kostya96.08@mail.ru>
2020-11-03 23:01:48 +0300

    dependencies clearing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,19 +50,19 @@ android {
 
 dependencies {
     api(platform(project(":versions")))
-    kapt(platform(project(":versions")))
     api(project(":network"))
 
-    implementation(project(":shared"))
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(project(":shared"))
     implementation(project(":spotify-app-remote"))
 
     implementation(Libs.CORE_KTX)
+    implementation(Libs.GSON)
 
     //UI
+    implementation(Libs.ACTIVITY_KTX)
     implementation(Libs.ADAPTER_DELEGATES)
     implementation(Libs.ADAPTER_DELEGATES_VIEW_BINDING)
-    implementation(Libs.ACTIVITY_KTX)
     implementation(Libs.APPCOMPAT)
     implementation(Libs.FRAGMENT_KTX)
     implementation(Libs.CARDVIEW)
@@ -72,28 +72,17 @@ dependencies {
 
     // Architecture Components
     implementation(Libs.LIFECYCLE_LIVE_DATA_KTX)
-    testImplementation(Libs.ARCH_TESTING)
     implementation(Libs.NAVIGATION_FRAGMENT_KTX)
     implementation(Libs.ROOM_KTX)
     implementation(Libs.ROOM_RUNTIME)
     implementation(Libs.ROOM_COMMON)
     implementation(Libs.PREFERENCES)
-    kapt(Libs.ROOM_COMPILER)
     testImplementation(Libs.ROOM_KTX)
     testImplementation(Libs.ROOM_RUNTIME)
 
     // Dagger Hilt
     implementation(Libs.HILT_ANDROID)
     implementation(Libs.HILT_VIEWMODEL)
-    kapt(Libs.HILT_COMPILER)
-    kapt(Libs.ANDROIDX_HILT_COMPILER)
-    kaptAndroidTest(Libs.HILT_COMPILER)
-    kaptAndroidTest(Libs.ANDROIDX_HILT_COMPILER)
-
-    //Network
-    implementation(Libs.RETROFIT)
-    implementation(Libs.RETROFIT_GSON)
-    implementation(Libs.JSOUP)
 
     // Glide
     implementation(Libs.GLIDE)
@@ -105,17 +94,23 @@ dependencies {
     //Spotify
     implementation(Libs.SPOTIFY_AUTH)
 
+    kapt(platform(project(":versions")))
+    kapt(Libs.ROOM_COMPILER)
+    kapt(Libs.HILT_COMPILER)
+    kapt(Libs.ANDROIDX_HILT_COMPILER)
+
     // Instrumentation tests
     androidTestImplementation(Libs.ESPRESSO_CORE)
     androidTestImplementation(Libs.ESPRESSO_CONTRIB)
     androidTestImplementation(Libs.EXT_JUNIT)
 
     // Local unit tests
+    testImplementation(Libs.ARCH_TESTING)
     testImplementation(Libs.JUNIT)
     testImplementation(Libs.MOCKITO_CORE)
     testImplementation(Libs.MOCKITO_KOTLIN)
     testImplementation(Libs.HAMCREST)
 
-    // Solve conflicts with gson. DataBinding is using an old version.
-    implementation(Libs.GSON)
+    kaptAndroidTest(Libs.HILT_COMPILER)
+    kaptAndroidTest(Libs.ANDROIDX_HILT_COMPILER)
 }
