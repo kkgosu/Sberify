48251c4cc313f215f9419b9393d72ee702b2aaa7
kkgosu <driim@outlook.com>
2020-12-20 15:26:35 +0300

    parcel

--- app/src/main/java/com/example/sberify/models/newdomain/TrackDomainModel.kt
+++ app/src/main/java/com/example/sberify/models/newdomain/TrackDomainModel.kt
@@ -1,11 +1,14 @@
 package com.example.sberify.models.newdomain
 
+import android.os.Parcelable
 import com.example.sberify.models.domain.BaseModel
+import kotlinx.android.parcel.Parcelize
 
 /**
  * @author Konstantin Koval
  * @since 06.12.2020
  */
+@Parcelize
 data class TrackDomainModel(
     val id: String,
     val name: String,
@@ -17,4 +20,4 @@ data class TrackDomainModel(
     var isFavorite: Boolean,
     val previewUri: String,
     val markets: List<String>
-) : BaseModel(id)
+) : BaseModel(id), Parcelable
