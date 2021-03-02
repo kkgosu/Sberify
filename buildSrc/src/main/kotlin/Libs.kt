5a07a0a2f9de6abd5abbec0f87c7f019210946cf
kkgosu <driim@outlook.com>
2021-03-02 23:31:11 +0300

    Upgraded com.google.android.material:material from 1.3.0-alpha03 to 1.3.0

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -26,7 +26,7 @@ object Libs {
     const val LIFECYCLE_LIVE_DATA_KTX = "androidx.lifecycle:lifecycle-livedata-ktx:2.3.0-beta01"
     const val LIFECYCLE_VIEW_MODEL_KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0-beta01"
     const val LOTTIE = "com.airbnb.android:lottie:3.6.0"
-    const val MATERIAL = "com.google.android.material:material:1.3.0-alpha03"
+    const val MATERIAL = "com.google.android.material:material:1.3.0"
     const val MOCKITO = "io.mockk:mockk:1.10.5"
     const val NAVIGATION_FRAGMENT_KTX = "androidx.navigation:navigation-fragment-ktx:2.3.2"
     const val PREFERENCES = "androidx.preference:preference-ktx:1.1.1"
