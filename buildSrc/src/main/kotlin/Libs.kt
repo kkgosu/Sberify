2f8e2b6b17c70271f91a14798c1b6ede8665ad08
kkgosu <driim@outlook.com>
2021-04-29 23:23:16 +0300

    Upgraded androidx.lifecycle:lifecycle-viewmodel-ktx from 2.3.0 to 2.3.1

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -24,7 +24,7 @@ object Libs {
     const val JSOUP = "org.jsoup:jsoup:1.10.1"
     const val JUNIT = "junit:junit:4.13.2"
     const val LIFECYCLE_LIVE_DATA_KTX = "androidx.lifecycle:lifecycle-livedata-ktx:2.3.1"
-    const val LIFECYCLE_VIEW_MODEL_KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0"
+    const val LIFECYCLE_VIEW_MODEL_KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1"
     const val LOTTIE = "com.airbnb.android:lottie:3.6.1"
     const val MATERIAL = "com.google.android.material:material:1.3.0"
     const val MOCKITO = "io.mockk:mockk:1.10.5"
