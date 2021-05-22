f0734467defabdaaf7e74ac2743c3c3b8c1bb7a3
kkgosu <driim@outlook.com>
2021-05-22 13:58:21 +0300

    Upgraded androidx.room:room-compiler from 2.2.6 to 2.3.0

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -33,7 +33,7 @@ object Libs {
     const val PREFERENCES = "androidx.preference:preference-ktx:1.1.1"
     const val RETROFIT = "com.squareup.retrofit2:retrofit:2.9.0"
     const val RETROFIT_GSON = "com.squareup.retrofit2:converter-gson:2.9.0"
-    const val ROOM_COMPILER = "androidx.room:room-compiler:2.2.6"
+    const val ROOM_COMPILER = "androidx.room:room-compiler:2.3.0"
     const val ROOM_COMMON = "androidx.room:room-common:2.2.6"
     const val ROOM_KTX = "androidx.room:room-ktx:2.2.6"
     const val ROOM_RUNTIME = "androidx.room:room-runtime:2.3.0"
