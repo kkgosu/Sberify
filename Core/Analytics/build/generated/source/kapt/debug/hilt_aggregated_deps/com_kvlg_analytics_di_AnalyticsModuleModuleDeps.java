07aa5864992f4c0e4075c2f2ef369b0546766016
kkgosu <driim@outlook.com>
2021-09-24 20:36:46 +0300

    Upgraded com.spotify.android:auth from 1.2.3 to 1.2.5

---
+++ Core/Analytics/build/generated/source/kapt/debug/hilt_aggregated_deps/com_kvlg_analytics_di_AnalyticsModuleModuleDeps.java
@@ -0,0 +1,13 @@
+package hilt_aggregated_deps;
+
+import dagger.hilt.processor.internal.aggregateddeps.AggregatedDeps;
+
+/**
+ * Generated class to pass information through multiple javac runs.
+ */
+@AggregatedDeps(
+    components = "dagger.hilt.components.SingletonComponent",
+    modules = "com.kvlg.analytics.di.AnalyticsModule"
+)
+class com_kvlg_analytics_di_AnalyticsModuleModuleDeps {
+}
